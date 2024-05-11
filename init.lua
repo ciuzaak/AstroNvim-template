e2b6124eb8ced2e2a48b382ccf4d3137862f02a7
Ciuzaak Wong <as460230379@hotmail.com>
2024-05-11 11:46:41 +0800

    customize

--- init.lua
+++ init.lua
@@ -17,3 +17,12 @@ end
 
 require "lazy_setup"
 require "polish"
+
+vim.cmd.colorscheme "catppuccin-latte"
+vim.keymap.set('i', '<C-]>', 'copilot#Accept("<CR>")', {
+  expr=true,
+  replace_keycodes=false
+})
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_filetypes = {["*"] = true}
